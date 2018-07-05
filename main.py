babc860e6fb7679aaa634736f76a913f9f433775
Dmitry Fisenko <fisenkodv@gmail.com>
2018-07-04 22:53:41 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+from __future__ import print_function
+import os
+
+
+class GeotagKeywordsCleaner(object):
+    def __init__(self, source_photos_path):
+        self.source_photos_path = source_photos_path
+
+    def cleanup(self):
+        return
+
+
+if __name__ == '__main__':
+    source_photos_path = input('Source path: ') or ''
+    if source_photos_path:
+        cataloguer = GeotagKeywordsCleaner(source_photos_path)
+        cataloguer.cleanup()
+    else:
+        pass
